41b8c3bcc36e5439ba7f069f26775e9bd561fe5b
Yurii Soroka <yurisoroka165@gmail.com>
2023-06-05 11:57:56 +0300

    ok

--- Screens/PostsScreen/PostsScreen.js
+++ Screens/PostsScreen/PostsScreen.js
@@ -1,13 +1,23 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { styles } from "./PostsScreenStyles";
+import LogoutButton from "../../components/LogoutButton";
+import AppControls from "../../components/AppControls";
+import AuthenticatedUserInfo from "../../components/AuthenticatedUserInfo";
 
 const PostsScreen = () => {
+    const handleLogoutPress = () => {
+        console.log("Logout");
+    };
+
     return (
         <View style={styles.postsScreenContainer}>
             <View style={styles.postsScreenHeaderContainer}>
-
+                <Text style={styles.postsScreenHeader}>Публікації</Text>
+                <LogoutButton onPress={handleLogoutPress}></LogoutButton>
             </View>
+            <AuthenticatedUserInfo />
+            <AppControls />
         </View>
     );
 };
--- Screens/PostsScreen/PostsScreenStyles.js
+++ Screens/PostsScreen/PostsScreenStyles.js
@@ -7,13 +7,25 @@ export const styles = StyleSheet.create({
         backgroundColor: "#ffffff",
     },
     postsScreenHeaderContainer: {
+        position: "relative",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
         width: "100%",
         height: 90,
         paddingLeft: 16,
         paddingRight: 16,
+        paddingBottom: 12,
         borderStyle: "solid",
         borderColor: "#E8E8E8",
         borderBottomWidth: 1,
         // backgroundColor: "#000000"
+    },
+    postsScreenHeader: {
+        marginLeft: "auto",
+        marginRight: "auto",
+        fontWeight: 500,
+        fontSize: 17,
     },
 });
